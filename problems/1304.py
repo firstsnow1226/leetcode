6140d9b5421d8184dd9db4251974aef0d263bef5
firstsnow1226 <itr802222@gmail.com>
2025-09-07 12:36:02 +0800

    1304

---
+++ problems/1304.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        ans = []
+        if n % 2 == 1:
+            ans.append(0)
+        for i in range(n//2):
+            ans.append(i+1)
+            ans.append(-i-1)
+        return ans
