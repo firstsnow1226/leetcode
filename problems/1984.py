7529eb9d996a28aec23e8898bbaa1815ccdbf8f6
firstsnow1226 <itr802222@gmail.com>
2026-01-25 17:26:27 +0800

    1984

---
+++ problems/1984.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        ans = nums[k-1] - nums[0]
+        for i in range(len(nums)-k+1):
+            ans = min(ans, nums[i+k-1]-nums[i])
+        return ans
+
