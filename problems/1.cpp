6642d5cec4f31e2236afe86b5d3aee94be90d2ff
firstsnow1226 <itr802222@gmail.com>
2026-03-06 21:55:59 +0800

    1758

---
+++ problems/1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int N = nums.size();
+
+        for(int i=0;i<N;i++){
+            for(int j=i+1;j<N;j++){
+                if(nums[i]+nums[j] == target){
+                    vector<int> ans={i,j};
+                    return ans;
+                }
+            }
+        }
+        vector<int> ans={0,0};
+        return ans; 
+    }
+};
